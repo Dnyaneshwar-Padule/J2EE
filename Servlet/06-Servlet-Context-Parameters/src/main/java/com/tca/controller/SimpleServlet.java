0c4346a1401efe8b0263646523cd4e7efa809ca8
Dnyaneshwar-Padule <dnyaneshwarpadule83@gmail.com>
2026-04-03 22:27:02 +0530

    Servlet: Servlet-Context

--- Servlet/06-Servlet-Context-Parameters/src/main/java/com/tca/controller/SimpleServlet.java
+++ Servlet/06-Servlet-Context-Parameters/src/main/java/com/tca/controller/SimpleServlet.java
@@ -1,44 +1,36 @@
 package com.tca.controller;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/get")
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public SimpleServlet() {
-		System.out.println("SimpleServlet.SimpleServlet()");
-	}
-	
-	@Override
-	public void init() throws ServletException {
-		super.init();
-		System.out.println("init()");
-	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
 		response.setContentType("text/html");
+		ServletContext context  = getServletContext();
+		Enumeration<String> parms = context.getInitParameterNames();
 		PrintWriter out = response.getWriter();
-				
-		ServletConfig config = this.getServletConfig();
-		
-		Enumeration<String> parms = config.getInitParameterNames();
 		
 		while(parms.hasMoreElements()) {
 			String parameterName = parms.nextElement();
-			out.println(parameterName + " : " + config.getInitParameter(parameterName) + "<br>");
+			out.println(parameterName +  " : " + context.getInitParameter(parameterName) + "<br>");
 		}
 		
-		out.flush();
+		out.print("Server Info : " +  context.getServerInfo() + "<br>");
+		out.print("Major Version : " + context.getMajorVersion() + "<br>");
+		out.print("Minor Version : " + context.getMinorVersion() + "<br>");
+		
 		out.close();
 	}
 
